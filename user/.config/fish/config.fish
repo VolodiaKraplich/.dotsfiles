e758770443c54106f050fb0f3b9fffc5612307c1
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-05-27 20:01:17 +0300

    Format fish config files
    Run `fish_indent` to standardize formatting in the config files.

--- user/.config/fish/config.fish
+++ user/.config/fish/config.fish
@@ -75,21 +75,21 @@ end
 ## Functions for command history
 # Functions needed for !! and !$
 function __history_previous_command
-    switch (commandline -t)
+  switch (commandline -t)
     case "!"
-        commandline -t $history[1]; commandline -f repaint
+      commandline -t $history[1]; commandline -f repaint
     case "*"
-        commandline -i !
-    end
+      commandline -i !
+  end
 end
 
 function __history_previous_command_arguments
     switch (commandline -t)
-    case "!"
-        commandline -t ""
-        commandline -f history-token-search-backward
-    case "*"
-        commandline -i '$'
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
     end
 end
 
--- user/.config/fish/functions/mkcd.fish
+++ user/.config/fish/functions/mkcd.fish
@@ -1,3 +1,3 @@
 function mkcd --description "Create directory and cd into it"
-    mkdir -p $argv[1] && cd $argv[1]
+  mkdir -p $argv[1] && cd $argv[1]
 end
